eeda8a1a15373cea4d9cdafd8b2ed81f07a1c3e5
shuchao <645904877@qq.com>
2017-08-05 00:45:04 +0800

    ...

---
+++ router/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by shuc on 17/8/5.
+ */
+import KoaRouter from 'koa-router';
+
+const Router = new KoaRouter();
+
+export default Router;
---
+++ router/user/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by shuc on 17/8/1.
+ */
+
+import Router from './../index';
+
+Router.get();
