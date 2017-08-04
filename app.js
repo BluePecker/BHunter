2cfaca1cdfca22e82337de58c77ac9032fb325fd
shuchao <645904877@qq.com>
2017-08-04 23:45:58 +0800

    test

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 /**
  * Created by shuc on 17/8/1.
  */
+import Koa from 'koa';
+import Logger from 'koa-logger';
+import Config from 'config';
+
+const app = new Koa();
+
+app.use(Logger());
+
+app.use(ctx => {
+    ctx.body = 'Hello Koa';
+});
+
+app.listen(Config.get('Server.port'));
