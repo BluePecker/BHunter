79fe69122215a0a93ffa85c7561dd49c40098060
shuchao <645904877@qq.com>
2017-08-05 11:54:30 +0800

    connector

---
+++ bootstrap/connect.js
@@ -0,0 +1,18 @@
+/**
+ * Created by shuc on 17/8/5.
+ */
+
+class Connect {
+    // connect mysql
+    Mysql(opt) {
+    }
+    // connect redis
+    Redis(opt) {
+    }
+
+    // connect mongodb
+    Mongodb(opt) {
+    }
+}
+
+export default new Connect();
--- bootstrap/index.js
+++ bootstrap/index.js
@@ -4,12 +4,16 @@
 import Koa from 'koa';
 import Config from 'config';
 import Logger from 'koa-logger';
+import connect from './connect';
 
 /**
  * @class Bootstrap
  * @property {object} app
  */
 class Bootstrap {
+    /**
+     * construct func
+     */
     constructor() {
         // new app
         this.app = new Koa();
@@ -24,15 +28,43 @@ class Bootstrap {
         });
     }
 
+    /**
+     * starting program
+     */
     start() {
-        // just for test
+        // todo just for test
         this.app.use(ctx => {
             ctx.body = 'Hello Koa';
         });
+
+        // get db config
+        const database = Config.get('Database');
+        // connect db by config defined
+        this.connector(database);
+
         // get server port
         const port = Config.get('Server.port');
         // start to listen
         this.app.listen(port);
+    }
+
+    /**
+     * connector database
+     * @param opt
+     */
+    connector(opt) {
+        // connect db by opt
+        Object.keys(opt).forEach((conn) => {
+            // the first letter
+            conn = conn.toLowerCase();
+            const name = conn.replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+            // check has connector
+            if (!Reflect.has(connect, name)) {
+                throw new Error(`connector ${name} not defined`);
+            }
+            // instantiate the connection
+            this.app.context[name] = connect[name](opt);
+        });
     }
 }
 
