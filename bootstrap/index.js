5f0e369234cc17a28b69a90860b6a5f079e17c98
shuchao <645904877@qq.com>
2017-08-06 14:39:39 +0800

    logger

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -5,7 +5,8 @@ import Koa from 'koa';
 import Dir from 'node-dir';
 import Config from 'config';
 import Mount from 'koa-mount';
-import Logger from 'koa-logger';
+import Log4js from 'log4js';
+// import Logger from 'koa-logger';
 import Connect from './connect';
 import Router from '../router/router';
 
@@ -20,14 +21,26 @@ class Bootstrap {
     constructor() {
         // new app
         this.app = new Koa();
-        // use log middleware
-        this.app.use(Logger());
+        // // use log middleware
+        // this.app.use(Logger());
         // x-response-time
         this.app.use(async(ctx, next) => {
             const start = Date.now();
             await next();
             const ms = Date.now() - start;
             ctx.set('X-Response-Time', `${ms}ms`);
+        });
+    }
+
+    /**
+     * logger
+     */
+    static logger() {
+        const Logger = Config.has('Logger') ? Config.get('Logger') : {};
+
+        Log4js.configure({
+            appenders: Logger.appenders || {},
+            categories: Logger.categories || {}
         });
     }
 
@@ -39,15 +52,16 @@ class Bootstrap {
         const database = Config.get('Database');
         // connect db by config defined
         this.connector(database);
-
         // load router
         this.router();
-
+        // load logger
+        Bootstrap.logger();
         // get server port
         const port = Config.get('Server.port');
         // start to listen
         this.app.listen(port, () => {
-            // todo print start info
+            const logger = Log4js.getLogger();
+            logger.info(`listening ${port}`);
         });
     }
 
