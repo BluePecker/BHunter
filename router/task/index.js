4fb3dc74593c8ed96871f8c901f4ccba6dc521fd
shuchao <645904877@qq.com>
2017-08-09 00:18:07 +0800

    ignore test.js

--- router/task/index.js
+++ router/task/index.js
@@ -1,14 +1,4 @@
 /**
  * Created by shuc on 17/8/6.
  */
-import V1 from './v1';
-import Router from '../router';
-
-const Route = new Router();
-
-Route.get('/create', (ctx, next) => {
-    ctx.body = 'create';
-    next();
-});
-
-export {Route, V1};
+export default from './v1';
--- service/task/index.js
+++ service/task/index.js
@@ -1,10 +1,11 @@
 /**
  * Created by shuc on 17/8/6.
  */
+import Service from './index';
 import User from '../../model/mysql/user';
 import Task from '../../model/mongo/task';
 
-class TaskService {
+class TaskService extends Service {
     add = (ctx) => {
         User.sync({force: true}).then(() => {
             // Table created
