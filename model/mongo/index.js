bc2296b445c4ff5093ce5a6653faf63fad185e25
shuchao <645904877@qq.com>
2017-08-08 15:00:59 +0800

    remove some

--- model/mongo/index.js
+++ model/mongo/index.js
@@ -5,7 +5,6 @@ import Mongoose from 'mongoose';
 import Config from 'config';
 import Log4js from 'log4js';
 import Builder from 'mongodb-uri';
-// import Bluebird from 'bluebird';
 
 const options = Config.get('Database.mongo') || {};
 const auth = Builder.format(options);
