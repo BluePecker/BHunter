5ea970b29623a2a91b99b6ffd5ae858637005d8d
shuchao <645904877@qq.com>
2017-08-20 17:57:14 +0800

    fix bug

--- router/label/v1.js
+++ router/label/v1.js
@@ -1,3 +1,17 @@
 /**
  * Created by shuc on 17/8/20.
  */
+import Router from '../router';
+
+import Task from '../../service/business';
+
+const V1 = new Router({
+    prefix: '/label'
+});
+
+V1.post('/create', Task.create);
+V1.post('/list', Task.list);
+V1.post('/adopt', Task.adopt);
+V1.post('/reject', Task.reject);
+
+export default V1;
