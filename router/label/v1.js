d8e467c5835bf409f2be8be26da85da0e83a1207
shuchao <645904877@qq.com>
2017-08-20 17:58:43 +0800

    fix bug

--- router/label/v1.js
+++ router/label/v1.js
@@ -3,15 +3,15 @@
  */
 import Router from '../router';
 
-import Task from '../../service/business';
+import Label from '../../service/label';
 
 const V1 = new Router({
     prefix: '/label'
 });
 
-V1.post('/create', Task.create);
-V1.post('/list', Task.list);
-V1.post('/adopt', Task.adopt);
-V1.post('/reject', Task.reject);
+V1.post('/create', Label.create);
+V1.post('/list', Label.list);
+V1.post('/adopt', Label.adopt);
+V1.post('/reject', Label.reject);
 
 export default V1;
