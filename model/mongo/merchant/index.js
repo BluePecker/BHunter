82282408a93e43d4b9b6584832d4081c6a7b01e1
shuchao <645904877@qq.com>
2017-08-27 13:55:41 +0800

    fix bug

--- model/mongo/merchant/index.js
+++ model/mongo/merchant/index.js
@@ -90,8 +90,8 @@ Schema.statics = {
     // 下属商户
     own(user) {
         return this.find({
-            'owner'  : user,
-            'deleted': null
+            owner  : user,
+            deleted: null
         }, 'type review information').then(docs => {
             return docs.map(item => {
                 return {
