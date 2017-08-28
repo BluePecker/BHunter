ff763b9f0040e321376419ade6cdeedcb800b85d
shuchao <645904877@qq.com>
2017-08-29 01:21:15 +0800

    retry

--- model/redis/index.js
+++ model/redis/index.js
@@ -9,11 +9,26 @@ import Builder from 'parse-dburi';
 const logger = Log4js.getLogger('koa');
 const options = Config.get('Database.redis') || {};
 const auth = Builder.stringify(options);
-const redis = new Redis(auth, options.options || {});
 
-redis.connect(() => {
+// retry strategy
+const redis = new Redis(auth, options.options || {}, {
+    retryStrategy: (times) => {
+        "use strict";
+        if (times < ((options.options || {}).retries || 3)) {
+            // 500 ms
+            return 500;
+        }
+    }
+});
+
+redis.on('connect', () => {
     "use strict";
     logger.info(`REDIS: Successfully connected to ${auth}`);
+});
+
+redis.on('error', (err) => {
+    "use strict";
+    logger.info(`REDIS: Unable to connected for err -> ${err}`);
 });
 
 export default redis;
