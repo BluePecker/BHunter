df59e97346510774094c15a660ad924fa6240eb0
shuchao <645904877@qq.com>
2017-08-29 00:57:33 +0800

    fix bug

--- model/redis/index.js
+++ model/redis/index.js
@@ -1,3 +1,19 @@
 /**
  * Created by shuc on 17/8/7.
  */
+import Config from 'config';
+import Redis from 'ioredis';
+import Log4js from 'log4js';
+import Builder from 'parse-dburi';
+
+const logger = Log4js.getLogger('koa');
+const options = Config.get('Database.redis') || {};
+const auth = Builder.stringify(options);
+const redis = new Redis(auth, options.options || {});
+
+redis.connect(() => {
+    "use strict";
+    logger.info(`REDIS: Successfully connected to ${auth}`);
+});
+
+export default redis;
