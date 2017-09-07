6548ad7363ae66268cf6a65ededb1d42c1c0b103
shuchao <645904877@qq.com>
2017-09-07 20:39:04 +0800

    debug

--- middleware/boot.js
+++ middleware/boot.js
@@ -7,12 +7,12 @@ import mongoose from 'mongoose';
 class Boot {
 
     authentication = (ctx, next) => {
-        if (!ctx.headers['json-web-token']) {
-            ctx.throw(403, 'auth failed.');
-        } else {
-            // ctx.user = {_id: mongoose.Types.ObjectId(ctx.header['json-web-token'])};
+        // if (!ctx.headers['json-web-token']) {
+        //     ctx.throw(403, 'auth failed.');
+        // } else {
+            ctx.user = {_id: mongoose.Types.ObjectId(ctx.header['json-web-token'])};
             next();
-        }
+        // }
     };
 
     header = async(ctx, next) => {
