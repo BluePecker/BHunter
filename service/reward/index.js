941c2fcfa0efbdd6409631c5e883cceb8472ab30
shuchao <645904877@qq.com>
2017-09-17 17:29:49 +0800

    remove some field

--- service/reward/index.js
+++ service/reward/index.js
@@ -119,7 +119,7 @@ class RewardService extends Service {
             }, {
                 page  : ctx.params.page || 1,
                 sort  : req.sort || {},
-                select: 'describe headline contact tactics detail annex deadline location'
+                select: 'describe headline contact tactics detail deadline location'
             });
         }).then(paginate => {
             this.success(paginate);
